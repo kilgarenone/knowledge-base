df15ddaf187900ca60a0f4f2dbf6ad830c7711dc
kilgarenone <kwei88@gmail.com>
2022-08-06 10:44:42 +0800

    longest prefix algo

---
+++ algo/longestPrefix.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  if (strs.length === 0) return "";
+  let r = "";
+
+  // just take the first word and loop it
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[0][i] !== strs[j][i]) return r;
+    }
+    r += strs[0][i];
+  }
+  return r;
+};
